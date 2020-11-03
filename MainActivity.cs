d6eff5a8fbe1b54d637fc1d249775825e666a567
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2020-11-03 17:21:00 +0800

    added comments

--- MainActivity.cs
+++ MainActivity.cs
@@ -18,12 +18,18 @@ namespace SPMI_CHD
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
 
+            //akong gideclare ang url sa official website
             string sourceURL = "https://spmi-health-declaration.vercel.app";
+
+            //akong gitawag ang id sa webview gikan sa xml
             webView1 = FindViewById<WebView>(Resource.Id.webView1);
 
+            //akong gitawag ang internal class na WebViewClientClass
             webView1.SetWebViewClient(new WebViewClientClass());
+            //gipasa dire ang url string na ako gideclara
             webView1.LoadUrl(sourceURL);
 
+            //gi configure ang javascript para mugana sa akong application
             WebSettings websettings = webView1.Settings;
             websettings.JavaScriptEnabled = true;
 
--- obj/Debug/90/android/environment.x86.s
+++ obj/Debug/90/android/environment.x86.s
@@ -71,7 +71,7 @@ mono_aot_mode_name:
 	.section	.rodata.env.str.8,"aMS",@progbits,1
 	.type	.L.env.str.8, @object
 .L.env.str.8:
-	.asciz	"50919371-db90-4de8-ab8a-03c61d34bf82"
+	.asciz	"cb5ee749-70c9-46f5-89fc-05f096d1d3c4"
 	.size	.L.env.str.8, 37
 	.section	.rodata.env.str.9,"aMS",@progbits,1
 	.type	.L.env.str.9, @object
