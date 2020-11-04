da57ab33e323579aa11366b6d87ad7cc8ca1ee8e
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2020-11-04 12:02:00 +0800

    added textview and font origin

--- MainActivity.cs
+++ MainActivity.cs
@@ -20,14 +20,28 @@ namespace SPMI_CHD
 
             var txtaddress = FindViewById<TextView>(Resource.Id.txtaddress);
             var txttitle = FindViewById<TextView>(Resource.Id.txttitle);
+            var txtcopyright = FindViewById<TextView>(Resource.Id.txtcopyright);
             var btngoto = FindViewById<Button>(Resource.Id.btngoto);
+            var txtBibleverse = FindViewById<TextView>(Resource.Id.txtBibleverse);
+            var txtBookof = FindViewById<TextView>(Resource.Id.txtBookof);
 
             Typeface font1 = Typeface.CreateFromAsset(Assets, "Poppins-Thin.ttf");
             txtaddress.SetTypeface(font1, TypefaceStyle.Normal);
+
             Typeface font2 = Typeface.CreateFromAsset(Assets, "Poppins-Medium.ttf");
             txttitle.SetTypeface(font2, TypefaceStyle.Normal);
-            Typeface font3 = Typeface.CreateFromAsset(Assets, "Poppins-Regular.ttf");
-            btngoto.SetTypeface(font3, TypefaceStyle.Normal);
+
+            Typeface font3 = Typeface.CreateFromAsset(Assets, "Poppins-Light.ttf");
+            txtcopyright.SetTypeface(font3, TypefaceStyle.Normal);
+
+            Typeface font4 = Typeface.CreateFromAsset(Assets, "Poppins-Regular.ttf");
+            btngoto.SetTypeface(font4, TypefaceStyle.Normal);
+
+            Typeface font5 = Typeface.CreateFromAsset(Assets, "Poppins-LightItalic.ttf");
+            txtBibleverse.SetTypeface(font5, TypefaceStyle.Normal);
+
+            Typeface font6 = Typeface.CreateFromAsset(Assets, "Poppins-Regular.ttf");
+            txtBookof.SetTypeface(font6, TypefaceStyle.Normal);
 
             Button btnGoto = FindViewById<Button>(Resource.Id.btngoto);
             btnGoto.Click += delegate { StartActivity(typeof(WebApplicationView)); };
